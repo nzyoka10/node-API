3eb2bd89baa2941547d613a77fa627bb3d8243c7
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-05-13 14:41:39 +0300

    adding a HTTP header

--- demo_http_url.js
+++ demo_http_url.js
@@ -3,13 +3,18 @@ var http = require('http');
 
 // Creating an HTTP server using the createServer method
 http.createServer(function (req, res) {
-    
-  // Adding an HTTP header to the response to specify the content type as HTML
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
-  // Writing the response body with the message "Hello World!"
-  res.write('Hello World!');
-  
-  // Ending the response
-  res.end();
+
+    // Adding an HTTP header to the response to specify the content type as HTML
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    // Writing the response body with the message "Hello World!"
+    //   res.write('Hello World!');
+
+    // Writing the request URL as the response body
+    res.write(req.url);
+
+    // Ending the response
+    res.end();
 }).listen(8080); // Listening on port 8080 for incoming requests
